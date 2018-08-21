2eb9f77c4ebafaad1cf470e44972fcdd6ca040d0
SonalGolani <42235838+SonalGolani@users.noreply.github.com>
2018-08-21 09:51:00 +0530

    admin list commit

--- clms-frontend/src/app/components/admin-list/admin-list.component.ts
+++ clms-frontend/src/app/components/admin-list/admin-list.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+export interface PeriodicElement {
+  sno: number;
+  userId: string;
+  name: string;
+}
+
+const ADMIN_DATA: PeriodicElement[] = [
+  {sno: 1, userId: 'ronpradh', name: 'Ronit Pradhan'},
+  {sno: 2, userId: 'roshetty', name: 'Rohan Shetty'},
+];
+
 
 @Component({
   selector: 'app-admin-list',
   templateUrl: './admin-list.component.html',
   styleUrls: ['./admin-list.component.css']
 })
-export class AdminListComponent implements OnInit {
+export class AdminListComponent implements OnInit{
 
-  constructor() { }
+  displayedColumns: string[] = ['sno', 'userId', 'name'];
+  dataSource = ADMIN_DATA;
+
+  addAdmin: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder) {
+    this.addAdmin = this.formBuilder.group({
+      username: ['', Validators.required]
+    });
+  }
 
   ngOnInit() {
   }
+
+  onSubmit(){
+    const val = this.addAdmin.value;
+    console.log("Leave applied successfully! :D");
+    console.log("from: "+val.username);
+  }  
 
 }
